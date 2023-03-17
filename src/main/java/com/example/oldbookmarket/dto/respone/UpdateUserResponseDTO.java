8d4befa9e03b03ed55f5245a5676a1c438c3378b
HoVinhDuySE140985 <74530781+HoVinhDuySE140985@users.noreply.github.com>
2023-03-17 20:31:49 +0700

    sửa lỗi api post tạo book api tạo api đăng nhập đk

---
+++ src/main/java/com/example/oldbookmarket/dto/respone/UpdateUserResponseDTO.java
@@ -0,0 +1,23 @@
+package com.example.oldbookmarket.dto.respone;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UpdateUserResponseDTO {
+//    private Long id;
+    private String name;
+    private Integer phoneNumber;
+    private String email;
+    private String gender;
+    private String imageUrl;
+    private LocalDate dob;
+    private String password;
+}
