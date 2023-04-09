e0df8e7401a12c8a01835d4424eb34ac0591def4
HoVinhDuySE140985 <74530781+HoVinhDuySE140985@users.noreply.github.com>
2023-04-09 23:26:33 +0700

    update api payment

--- src/main/java/com/example/oldbookmarket/shared/utils/Common.java
+++ src/main/java/com/example/oldbookmarket/shared/utils/Common.java
@@ -8,7 +8,8 @@ import lombok.NoArgsConstructor;
 public class Common {
 
     public static final String IPN_URL_MOMO = "http://localhost:8080/api/v1/MomoConfirm";
-    public static final String REDIRECT_URL_MOMO = "http://localhost:8080/api/v1/MomoConfirm";
+    public static final String REDIRECT_URL_MOMO = "https://lobster-app-uadur.ondigitalocean.app/api/v1/MomoConfirm";
+//    public static final String REDIRECT_URL_MOMO = "thay bằng link trang muốn chuyển hướng tới sau khi thanh toán";
     public static final String PARTNER_CODE = "MOMOM1IH20220922";
     public static final String ACCESS_KEY = "CXSZrRmGBowTzMUv";
     public static final String SECRET_KEY = "S8vHx9fepIhC9Sr4lML7C5Graf4w5k2p";
