09b116d513948cf602f16c6f4e4b3ef06398ad29
HoVinhDuySE140985 <74530781+HoVinhDuySE140985@users.noreply.github.com>
2023-05-02 21:29:31 +0700

    fix config

--- src/main/java/com/example/oldbookmarket/service/serviceimplement/ScheduledConfig.java
+++ src/main/java/com/example/oldbookmarket/service/serviceimplement/ScheduledConfig.java
@@ -37,7 +37,7 @@ public class ScheduledConfig {
     @Autowired
     ComplaintRepo complaintRepo;
 
-    @Scheduled(cron = "0 0 12 * * ? " , zone = "Asia/Ho_Chi_Minh")
+    @Scheduled(cron = "0 0 0 ? * * " , zone = "Asia/Ho_Chi_Minh")
 //    @Scheduled(fixedDelay = 1600000)
     public void load5sForCheckOrderStatus(){
         List<Order> orderList = orderRepo.findAll();
